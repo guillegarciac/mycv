7a0a9d27458ff1cb446cf69b0ecedf0d0be5ccab
guille garcia <ggarciacardiel@gmail.com>
2024-09-02 20:05:10 +0200

    updates qualifications to include udemy coruses

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -51,62 +51,165 @@ const Qualification = () => {
         </div>
       </div>
       <div className="qualification__container container">
-      <div className="qualification__sections">
-        {/* Education */}
+        <div className="qualification__sections">
+          {/* Education */}
 
-        <div
-          className={
-            toggleState === 1
-              ? "qualification__content qualification__content-active"
-              : "qualification__content"
-          }
-        >
-          <div>
-          <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Microservices with Node JS and React
-                </h3>
-                <span className="qualification__subtitle">Udemy</span>
-                <div className="qualification__calendar">
-                  <HiOutlineCalendar className="qualification__calendar-icon" />
-                  Studying 2024
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div>
+              <div className="qualification__data">
+                <div>
+                  <h3 className="qualification__title">
+                    Microservices with NodeJS, Docker & Kubernetes
+                  </h3>
+                  <span className="qualification__subtitle">Udemy</span>
+                  <div className="qualification__calendar">
+                    <HiOutlineCalendar className="qualification__calendar-icon" />
+                    Studying 2024
+                  </div>
+                </div>
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
                 </div>
               </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
+              <div className="qualification__data">
+                <div></div>
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+                <div>
+                  <h3 className="qualification__title">
+                    Certified Cloud Practitioner CLF-C02
+                  </h3>
+                  <span className="qualification__subtitle">
+                    Amazon Web Services
+                  </span>
+                  <div className="qualification__calendar">
+                    <HiOutlineCalendar className="qualification__calendar-icon" />
+                    Studying 2024
+                  </div>
+                </div>
               </div>
+              <a
+                href="https://www.credential.net/efe5d557-8a09-4546-be18-66ce6e48c46a#gs.dzxz7q"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="qualification__link"
+              >
+                <div className="qualification__data">
+                  <div>
+                    <h3 className="qualification__title">
+                      Software Engineering, Web Development
+                    </h3>
+                    <span className="qualification__subtitle">Ironhack</span>
+                    <div className="qualification__calendar">
+                      <HiOutlineCalendar className="qualification__calendar-icon" />
+                      2023
+                    </div>
+                  </div>
+                  <div>
+                    <span className="qualification__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                </div>
+              </a>
+              <a
+                href="https://shiji.udemy.com/certificate/UC-497083d6-c242-4cb0-b0ba-8bdbe0ed43dd/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="qualification__link"
+              >
+                <div className="qualification__data">
+                  <div></div>
+                  <div>
+                    <span className="qualification__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                  <div>
+                    <h3 className="qualification__title">
+                      Project Planning with Advanded Roadmaps for Jira
+                    </h3>
+                    <span className="qualification__subtitle">
+                      Udemy
+                    </span>
+                    <div className="qualification__calendar">
+                      <HiOutlineCalendar className="qualification__calendar-icon" />
+                      2022
+                    </div>
+                  </div>
+                </div>
+              </a>
+
+              <a
+                href="https://www.udemy.com/certificate/UC-6bb2528c-d611-4f84-b3b8-0bd0f0e62eec/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="qualification__link"
+              >
+                <div className="qualification__data">
+                  <div>
+                    <h3 className="qualification__title">
+                      Certified Scrum Product Owner (CSPO)
+                    </h3>
+                    <span className="qualification__subtitle">Udemy</span>
+                    <div className="qualification__calendar">
+                      <HiOutlineCalendar className="qualification__calendar-icon" />
+                      2020
+                    </div>
+                  </div>
+                  <div>
+                    <span className="qualification__rounder"></span>
+                    <span className="qualification__line"></span>
+                  </div>
+                </div>
+              </a>
             </div>
-            <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
+
+            <a
+              href="https://www.linkedin.com/in/guilletheproductmanager/overlay/50981034/single-media-viewer/?profileId=ACoAAA5torYBowPQtRvPN92L4thaj9hkqDLDtIU"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="qualification__link"
+            >
+              <div className="qualification__data">
+                <div></div>
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+                <div>
+                  <h3 className="qualification__title">
+                    Master Certificate in Financial Revenue Management
+                  </h3>
+                  <span className="qualification__subtitle">
+                    Cornell University
+                  </span>
+                  <div className="qualification__calendar">
+                    <HiOutlineCalendar className="qualification__calendar-icon" />
+                    2016
+                  </div>
+                </div>
               </div>
+            </a>
+
+            <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
-                  Certified Cloud Practitioner CLF-C02
+                  Bachelor’s Degree in Tourism and Hospitality Management
                 </h3>
                 <span className="qualification__subtitle">
-                  Amazon Web Services
+                  Universitat Ramon Llull
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
-                  Studying 2024
-                </div>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Software Engineering, Web Development
-                </h3>
-                <span className="qualification__subtitle">Ironhack</span>
-                <div className="qualification__calendar">
-                  <HiOutlineCalendar className="qualification__calendar-icon" />
-                  2023
+                  2014
                 </div>
               </div>
               <div>
@@ -114,47 +217,6 @@ const Qualification = () => {
                 <span className="qualification__line"></span>
               </div>
             </div>
-          </div>
-
-          <div className="qualification__data">
-            <div></div>
-            <div>
-              <span className="qualification__rounder"></span>
-              <span className="qualification__line"></span>
-            </div>
-            <div>
-              <h3 className="qualification__title">
-                Master Certificate in Financial Revenue Management
-              </h3>
-              <span className="qualification__subtitle">
-                Cornell University
-              </span>
-              <div className="qualification__calendar">
-                <HiOutlineCalendar className="qualification__calendar-icon" />
-                2016
-              </div>
-            </div>
-          </div>
-
-          <div className="qualification__data">
-            <div>
-              <h3 className="qualification__title">
-                Bachelor’s Degree in Tourism and Hospitality Management
-              </h3>
-              <span className="qualification__subtitle">
-                Universitat Ramon Llull
-              </span>
-              <div className="qualification__calendar">
-                <HiOutlineCalendar className="qualification__calendar-icon" />
-                2014
-              </div>
-            </div>
-            <div>
-              <span className="qualification__rounder"></span>
-              <span className="qualification__line"></span>
-            </div>
-          </div>
-
           </div>
 
           {/* Experience */}
@@ -168,8 +230,12 @@ const Qualification = () => {
           >
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Director of Product Development</h3>
-                <span className="qualification__subtitle">Shiji Guest Solutions (Shiji Group)</span>
+                <h3 className="qualification__title">
+                  Director of Product Development
+                </h3>
+                <span className="qualification__subtitle">
+                  Shiji Guest Solutions (Shiji Group)
+                </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
                   2021 - Present
@@ -189,7 +255,7 @@ const Qualification = () => {
               <div>
                 <h3 className="qualification__title">Product Team Lead</h3>
                 <span className="qualification__subtitle">
-                Shiji Guest Solutions (Shiji Group)
+                  Shiji Guest Solutions (Shiji Group)
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
@@ -213,7 +279,7 @@ const Qualification = () => {
                 <span className="qualification__line"></span>
               </div>
             </div>
-            
+
             <div className="qualification__data">
               <div></div>
               <div>
@@ -222,7 +288,9 @@ const Qualification = () => {
               </div>
               <div>
                 <h3 className="qualification__title">Product Manager</h3>
-                <span className="qualification__subtitle">ReviewPro - Guest Intelligence</span>
+                <span className="qualification__subtitle">
+                  ReviewPro - Guest Intelligence
+                </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
                   2016 - 2018
@@ -233,7 +301,7 @@ const Qualification = () => {
               <div>
                 <h3 className="qualification__title">Key Account Manager</h3>
                 <span className="qualification__subtitle">
-                ReviewPro - Guest Intelligence
+                  ReviewPro - Guest Intelligence
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
@@ -254,7 +322,7 @@ const Qualification = () => {
               <div>
                 <h3 className="qualification__title">Junior Account Manager</h3>
                 <span className="qualification__subtitle">
-                ReviewPro - Guest Intelligence
+                  ReviewPro - Guest Intelligence
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
@@ -264,9 +332,11 @@ const Qualification = () => {
             </div>
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Assistant Hotel Manager</h3>
+                <h3 className="qualification__title">
+                  Assistant Hotel Manager
+                </h3>
                 <span className="qualification__subtitle">
-                Barceló Hotel Group
+                  Barceló Hotel Group
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
@@ -285,10 +355,10 @@ const Qualification = () => {
                 <span className="qualification__line"></span>
               </div>
               <div>
-                <h3 className="qualification__title">Chain Reservations Coordinator</h3>
-                <span className="qualification__subtitle">
-                citizenM Hotels
-                </span>
+                <h3 className="qualification__title">
+                  Chain Reservations Coordinator
+                </h3>
+                <span className="qualification__subtitle">citizenM Hotels</span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
                   2013 - 2014
@@ -297,9 +367,11 @@ const Qualification = () => {
             </div>
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Front Office Receptionist</h3>
+                <h3 className="qualification__title">
+                  Front Office Receptionist
+                </h3>
                 <span className="qualification__subtitle">
-                The Landmark London
+                  The Landmark London
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
@@ -320,7 +392,7 @@ const Qualification = () => {
               <div>
                 <h3 className="qualification__title">F&B Assistant</h3>
                 <span className="qualification__subtitle">
-                The Hotel Collection
+                  The Hotel Collection
                 </span>
                 <div className="qualification__calendar">
                   <HiOutlineCalendar className="qualification__calendar-icon" />
